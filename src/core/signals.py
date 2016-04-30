449d724327e50e8639db9182b6615b8fa0f47f29
Danny Milsom <dannysmilsom@gmail.com>
2016-04-30 10:13:11 +0100

    Update players win/loss count after a new game is recorded using a post save signal

--- src/core/signals.py
+++ src/core/signals.py
@@ -4,9 +4,22 @@ from django.dispatch import receiver
 
 from rest_framework.authtoken.models import Token
 
+from .models import Match
+
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     """Create a token associated with a user instance."""
     if created:
         Token.objects.create(user=instance)
+
+
+@receiver(post_save, sender=Match)
+def increment_player_counts(sender, instance=None, created=False, **kwargs):
+    """Increment the denormalized counts on the related player instances."""
+    if created:
+        instance.winner.total_win_count += 1
+        instance.winner.save()
+
+        instance.loser.total_loss_count += 1
+        instance.loser.save()
