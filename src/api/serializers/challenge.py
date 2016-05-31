cf79d0243eabc74f87537249b10ae056ac13f367
Danny Milsom <dannysmilsom@gmail.com>
2016-05-31 21:43:09 +0100

    Add some custom validation to challenge serializers
    * Very much a quick WIP

--- src/api/serializers/challenge.py
+++ src/api/serializers/challenge.py
@@ -17,13 +17,45 @@ class ChallengeSerializer(serializers.ModelSerializer):
             'id',
         )
 
+    def validate(self, data):
+        """Validates the data before it persists in the datastore."""
+        initiator = data.get('initiator')
+        challenger = data.get('challenger')
 
-        # validation!
+        # we only expect challenge instances to be set in the poolbot challenge
+        # command setup method, which should never try and set the player fields
+        if not self.instance:
+            if initiator or challenger:
+                raise serializers.ValidationError(
+                    "Cannot set player field when creating new challenge."
+                )
 
-        # custom things..
-        # cannot set challenger to the same as initiator
+        # once created, we need validate who the players are
+        else:
+            if self.instance.initiator and self.instance.challenger and self.instance.challenger == challenger:
+                raise serializers.ValidationError(
+                    "Initiator and challenger cannot be set to the same player."
+                )
 
-        # when updating, can only ever set challenger if inititator exists
-        # when updating, cannot set challenger to initiator
-        # when updating, allow setting initiator and challenger to none only if initiator is not null but last modified >15min or initaitor and challenge not null and last modified > 15min
+            if challenger and not self.instance.initiator:
+                raise serializers.ValidationError(
+                    "Cannot set challenger before a initiator has been set."
+                )
 
+            if initiator and self.instance.initiator:
+                raise serializers.ValidationError(
+                    "Cannot set a new challenge before the old one has been accepted."
+                )
+
+            # waiting for the match result to be recorded
+            if self.instance.challenger and self.instance.initiator and initiator:
+                raise serializers.ValidationError(
+                    "Cannot invoke a new challenge, current waiting for the match"
+                    "between {initiator} and {challenger} to be recorded."
+                    .format(
+                        initiator=self.instance.initiator,
+                        challenger=self.instance.challenger
+                    )
+                )
+
+        return data
