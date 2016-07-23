ee8b4e2a56b86fdb5ebdaac3207d21155f728218
Nathan Staines <nstaines@google.com>
2016-07-23 19:33:03 +0100

    Add 'player/elo/' API endpoint

--- src/api/views/player.py
+++ src/api/views/player.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 
 from core.models import Player, Match
-from core.utils import format_matches_to_show_form, form_cache_key
+from core.utils import format_matches_to_show_form, form_cache_key, calculate_elo
 
 from .base import TokenRequiredModelViewSet
 from ..serializers import PlayerSerializer, PatchPlayerSerializer
@@ -49,3 +49,35 @@ class PlayerViewSet(TokenRequiredModelViewSet):
         form = format_matches_to_show_form(limited_queryset, user)
 
         return Response(form)
+
+    @list_route(methods=['get'])
+    def elo(self, request, pk=None):
+        """Return the points that can be won/lost between two players."""
+        player1 = request.query_params.get('player1')
+        player2 = request.query_params.get('player2')
+
+        if player1 is None or player2 is None:
+            return None
+
+        player1_details = Player.objects.get(slack_id=player1)
+        player2_details = Player.objects.get(slack_id=player2)
+
+        result1 = calculate_elo(player1_details.elo, player2_details.elo, 1)
+        result2 = calculate_elo(player2_details.elo, player1_details.elo, 1)
+
+        def _generate_results(player_details, elo_win, elo_lose):
+            return {
+                'slack_id': player_details.slack_id,
+                'elo': player_details.elo,
+                'elo_win': elo_win,
+                'elo_lose': elo_lose,
+                'points_win': elo_win - player_details.elo,
+                'points_lose': player_details.elo - elo_lose,
+            }
+
+        results = [
+            _generate_results(player1_details, result1[0], result2[1]),
+            _generate_results(player2_details, result2[0], result1[1]),
+        ]
+
+        return Response(results)
