458ae77c8f4ce5ea0ecef9b2008bacfb8fb7da72
Danny Milsom <dannysmilsom@gmail.com>
2016-08-12 22:30:38 +0100

    Add a detail route for grannies at /api/player/<player_id>/grannies/

--- src/api/views/player.py
+++ src/api/views/player.py
@@ -50,3 +50,16 @@ class PlayerViewSet(TokenRequiredModelViewSet):
         form = format_matches_to_show_form(limited_queryset, user)
 
         return Response(form)
+
+    @detail_route(methods=['get'])
+    def grannies(self, request, pk=None):
+        """Return the games with a granny involving the specified player."""
+        player = self.get_object()
+
+        grannies_given = Match.objects.filter(winner=player, granny=True)
+        grannies_taken = Match.objects.filter(loser=player, granny=True)
+        all_granny_games = grannies_given | grannies_taken
+
+        all_granny_games = all_granny_games.order_by('-date')
+        serializer = MatchSerializer(all_granny_games, many=True)
+        return Response(serializer.data)
