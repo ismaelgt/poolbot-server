8abf10f0f5798261cd3cc7826b02d934b900a03f
Danny Milsom <dannysmilsom@gmail.com>
2016-09-04 15:14:10 +0100

    Add a season viewset API
    * Filerable by active flag.

--- src/api/urls.py
+++ src/api/urls.py
@@ -2,9 +2,10 @@
 
 from rest_framework import routers
 
-from .views import ChallengeViewSet, MatchViewSet, PlayerViewSet
+from .views import ChallengeViewSet, MatchViewSet, PlayerViewSet, SeasonViewSet
 
 api_router = routers.SimpleRouter()
 api_router.register(r'match', MatchViewSet)
 api_router.register(r'player', PlayerViewSet)
 api_router.register(r'challenge', ChallengeViewSet)
+api_router.register(r'season', SeasonViewSet)
---
+++ src/api/views/season.py
@@ -0,0 +1,15 @@
+from rest_framework import filters
+
+from core.models import Season
+
+from .base import TokenRequiredModelViewSet
+from ..serializers import SeasonSerializer
+
+
+class SeasonViewSet(TokenRequiredModelViewSet):
+
+    serializer_class = SeasonSerializer
+    queryset = Season.objects.all()
+    permissions = []
+    filter_backends = (filters.DjangoFilterBackend, filters.OrderingFilter)
+    filter_fields = ('active')
