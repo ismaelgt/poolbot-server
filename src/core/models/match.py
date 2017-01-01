fbd39f0ed4cb6d096d9328147cc8ef7be29c3314
Danny Milsom <dannysmilsom@gmail.com>
2017-01-01 16:40:49 +0000

    Add custom logic to Match model delete() method
    * Handles all the intricacies of the various denormalized fields across each model.

--- src/core/models/match.py
+++ src/core/models/match.py
@@ -3,6 +3,7 @@ from django.db import models
 from django.db.models import Q
 from django.utils import timezone
 
+from .elo_history import EloHistory
 from .season import Season
 
 
@@ -38,6 +39,59 @@ class Match(models.Model):
             self.date.date() > self.season.end_date
         ):
             raise ValidationError('The season does not correspond with date provided.')
+
+    def delete(self):
+        # """A match will only be deleted when it was created by mistake.
+        # When this happens, due to the high level of denormalization across
+        # different models, we need to carefully reset these fields to remove
+        # any notion of this match existing."""
+        from .season_player import SeasonPlayer
+
+        for player in (self.winner, self.loser):
+
+            # first we need to see how many elo points were won and loss
+            # TODO this should be simpler...
+            # Maybe the elo points won and lost could be stored on the match?
+            match_elo_history = self.elohistory_set.get(player=player)
+            previous_elo_history = EloHistory.objects.filter(
+                player=player,
+                date__lt=match_elo_history.date
+            ).order_by('-date').first()
+
+            # this might be their first game of the season...
+            previous_elo_history_score = (
+                1000 if
+                previous_elo_history is None else
+                previous_elo_history.elo_score
+            )
+
+            # update the denormalized points and counts on player and season player
+            if player is self.winner:
+                elo_diff = match_elo_history.elo_score - previous_elo_history_score
+
+                # update the denormalized player fields - one less win and less points
+                player.decrement_win_counts(commit=False)
+                player.decrement_elo_score(elo_diff)
+
+                # update the season player denormalized value
+                season_player = SeasonPlayer.objects.get(player=player, season=self.season)
+                season_player.undo_win(elo_diff)
+            else:
+                elo_diff = previous_elo_history_score - match_elo_history.elo_score
+
+                # update the denormalized player fields - one less loss and more points
+                player.decrement_loss_counts(commit=False)
+                player.increment_elo_score(elo_diff)
+
+                # update the season player denormalized value
+                season_player = SeasonPlayer.objects.get(player=player, season=self.season)
+                season_player.undo_loss(elo_diff)
+
+            # delete the elo history instance associated with the match
+            match_elo_history.delete()
+
+        # delete this instance once out of the loop
+        super(Match, self).delete()
 
     def save(self, *args, **kwargs):
         """Find which season the match should be associated with."""
