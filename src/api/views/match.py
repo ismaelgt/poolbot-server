6876703302072b7576099fddbbf7f51597d724cf
Danny Milsom <dannysmilsom@gmail.com>
2017-05-13 20:48:43 +0100

    Remove the hard coded limit of 10 most recent games for head-to-head endpoint

--- src/api/views/match.py
+++ src/api/views/match.py
@@ -32,6 +32,7 @@ class MatchViewSet(TokenRequiredModelViewSet):
         """Return the results of head to heads between two players."""
         player1 = request.query_params.get('player1')
         player2 = request.query_params.get('player2')
+        limit = int(request.query_params.get('limit', 10))
 
         if player1 is None or player2 is None:
             return None
@@ -40,15 +41,16 @@ class MatchViewSet(TokenRequiredModelViewSet):
         player2_wins = self.queryset.filter(winner=player2, loser=player1)
 
         # combine the two querysets, ordered by the most recent game,
-        # and put the last ten results in a nice string.
+        # and put the sliced results in a nice string.
         all_games = list(chain(player1_wins, player2_wins))
         ordered_games = sorted(all_games, key=attrgetter('date'), reverse=True)
-        limited_games = ordered_games[:10]
+        limited_games = ordered_games[:limit]
         serialized_matches = MatchSerializer(limited_games, many=True).data
 
         results = {
             player1: player1_wins.count(),
             player2: player2_wins.count(),
+            'total_count': len(all_games),
             'history': serialized_matches,
             'history_count': len(serialized_matches)
         }
